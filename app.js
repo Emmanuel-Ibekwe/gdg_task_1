23bafa2990d326aad18761ed9e353f56f5f1e486
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-08 13:01:21 +0100

    feat:added error middlware handler

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
 
+dotenv.config();
 const app = express();
 const { PORT } = process.env;
 
 app.use(bodyParser.json());
 
+app.use("/api/v1", authRoutes);
+
+app.use((error, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error);
+  }
+  const message = error.message || "Server error";
+  const status = error.status || 500;
+  res.status(status).json({ message: message });
+});
 app.listen(PORT, () => {
-  console.log(`server running at port${PORT}`);
+  console.log(`server running at port: ${PORT}`);
 });
